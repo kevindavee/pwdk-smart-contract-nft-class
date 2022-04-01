bd09375a4e3d6f01c7ef770790020fd6805d6d48
Kevin Dave <kevindavee1505@gmail.com>
2022-04-01 23:21:12 +0700

    update deployment script

--- test/utils.ts
+++ test/utils.ts
@@ -2,7 +2,11 @@ import { ethers } from "hardhat";
 
 export async function deployContract(name: string, symbol: string) {
   const erc721Starter = await ethers.getContractFactory("ERC721Starter");
-  const erc721StaterContract = await erc721Starter.deploy(name, symbol);
+  const erc721StaterContract = await erc721Starter.deploy(
+    name,
+    symbol,
+    "ipfs://myhash"
+  );
   await erc721StaterContract.deployed();
   return erc721StaterContract;
 }
