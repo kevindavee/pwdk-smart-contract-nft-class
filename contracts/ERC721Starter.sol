32f73988a9fc1baf2ac58a2ba4cdbe6e519ef60c
Kevin Dave <kevindavee1505@gmail.com>
2022-04-01 22:11:30 +0700

    add payable to mint method

--- contracts/ERC721Starter.sol
+++ contracts/ERC721Starter.sol
@@ -10,11 +10,15 @@ contract ERC721Starter is ERC721Enumerable {
 
     Counters.Counter private _tokenIds;
 
+    uint256 public constant PRICE = 0.1 ether;
+
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
 
     }
 
-    function mint() public {
+    function mint() public payable {
+        require(msg.value == PRICE, "ether must be same as price");
+
         uint256 currentTokenId = _tokenIds.current();
         _safeMint(msg.sender, currentTokenId);
         _tokenIds.increment();
