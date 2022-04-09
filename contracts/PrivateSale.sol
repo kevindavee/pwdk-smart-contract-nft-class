15ebceb9f7bef3da5072b8c3159b1dd697f31a7d
Kevin Dave <kevindavee1505@gmail.com>
2022-04-09 14:12:32 +0700

    add start and end timestamp

--- contracts/PrivateSale.sol
+++ contracts/PrivateSale.sol
@@ -4,13 +4,31 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateSale is Ownable {
+    uint256 public privateSaleStartTimestamp;
+    uint256 public privateSaleEndTimestamp;
+
     address[] private whitelistedAddresses;
     mapping(address => uint256) public addressToMintQty;
     mapping(address => bool) public addressToDoneMinting;
 
-    constructor() {}
+    constructor(uint256 _startTimestamp, uint256 _endTimestamp) {
+        privateSaleStartTimestamp = _startTimestamp;
+        privateSaleEndTimestamp = _endTimestamp;
+    }
 
-    function addAddressToWhitelist(address _address, uint256 _qty) public onlyOwner {
+    modifier duringPrivateSale() {
+        require(
+            block.timestamp >= privateSaleStartTimestamp &&
+                block.timestamp <= privateSaleEndTimestamp,
+            "private sale has ended or not started yet"
+        );
+        _;
+    }
+
+    function addAddressToWhitelist(address _address, uint256 _qty)
+        public
+        onlyOwner
+    {
         uint256 currentQty = addressToMintQty[_address];
         addressToMintQty[_address] = _qty;
         if (currentQty == 0) {
